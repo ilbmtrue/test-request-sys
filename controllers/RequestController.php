bc0edc8fb62cf6a138940dbf797bdbb3fa35c55b
ilbmtrue <ilbmtrue@yandex.ru>
2020-09-23 22:01:47 +0300

    add protection from guests

--- controllers/RequestController.php
+++ controllers/RequestController.php
@@ -1,25 +1,41 @@
 <?php
+
+// заглушка для не авторизованных пользователей сделал "в лоб", тк система не большая
+
 class RequestController
 {
     public function actionIndex(){
-        $requests = Request::getRequestsList();
-        require_once(ROOT . '/views/requests.php');
-        return true;
+        if(!User::isGuest()){
+            $requests = Request::getRequestsList();
+            require_once(ROOT . '/views/requests.php');
+            return true;
+        }
+        header("Location: /login");
+        return false;
     }
     public function actionIndexFilter(){
-        $params = array();
-        parse_str($_SERVER['QUERY_STRING'], $params);
-        $requests = Request::getRequestsList($params);
-        require_once(ROOT . '/views/requests.php');
-        return true;
+        if(!User::isGuest()){
+            $params = array();
+            parse_str($_SERVER['QUERY_STRING'], $params);
+            $requests = Request::getRequestsList($params);
+            require_once(ROOT . '/views/requests.php');
+            return true;
+        } 
+        header("Location: /login");
+        return false;
+        
     }
     public function actionEdit($id){
-        if (isset($_POST['edit'])) {
-            Request::updateRequest($id);
-        }
-        $request = Request::getRequestById($id);
-        require_once(ROOT . '/views/request.php');
-        return true;
+        if(!User::isGuest()){
+            if (isset($_POST['edit'])) {
+                Request::updateRequest($id);
+            }
+            $request = Request::getRequestById($id);
+            require_once(ROOT . '/views/request.php');
+            return true;
+        } 
+        header("Location: /login");
+        return false;
     }
     public function actionAdd()
     {
